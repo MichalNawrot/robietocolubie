79bbe444f43665641aaeae86f8c6b3ed82bc0cc9
Fergus Findley <fergus.findley@gmail.com>
2024-04-05 23:36:42 +0200

    make static frontpage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        josefin: ['"Josefin Sans"', ...defaultTheme.fontFamily.sans]
+      },
+      animation: {
+        'spin-slow': 'spin 10s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
